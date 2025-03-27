4c9c4474011528794fc2a98db2bb58cbee547b72
root <root@LAPTOP-UPVRKKQD.localdomain>
2025-03-27 12:40:56 +0800

    update

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,14 +19,13 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?; // ? 作用 若返回Err，则立刻返回，若返回Ok，则将qty设置为正常值
 
     Ok(qty * cost_per_item + processing_fee)
 }
--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
@@ -15,8 +14,11 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
-
+    let cost = total_cost(pretend_user_input);
+    let cost = match cost{
+        Ok(c)=>c,
+        Err(error)=>{ panic!("Problem with ParseInt: {:?}", error)}
+    };
     if cost > tokens {
         println!("You can't afford that many!");
     } else {
